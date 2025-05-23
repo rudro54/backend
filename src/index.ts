5d5ee09d7f231afdac166d4f8cf47ea1d8dddb3a
Abdullah Al Zayed <rudro54@gmail.com>
2025-05-22 21:24:19 -0500

    Added enve

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,15 @@ import router from "./routes/products";
 import productRouter from "./routes/products";
 import reviewRouter from "./routes/reviews";
 import bodyParser from "body-parser";
+
 const app = express();
-const port = 3000;
-app.use(cors())
+const port = process.env.PORT || 3000; // <--- THIS IS CRITICAL
+
+app.use(cors());
 app.use(express.json());
 app.use(reviewRouter);
 app.use(productRouter);
 
-
 app.listen(port, () => {
-  console.log(`Example app listening port http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
